41f89f6c7abd4326cd06ed181fa61810c7c15280
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-12-18 20:53:03 +0200

    feat(.bash_aliases_v2.sh) new function cert_check()

---
+++ .bash_aliases_v2.sh
@@ -0,0 +1,11 @@
+checkcert() {
+	# e.g. checkcert www.github.com
+	# this function from: https://stackoverflow.com/questions/21181231/server-certificate-verification-failed-cafile-etc-ssl-certs-ca-certificates-c/67698986#67698986
+	echo -n |
+		openssl s_client -showcerts -servername www.github.com \
+			-connect www.github.com:443 2>/dev/null |
+		tac |
+		awk '/-END CERTIFICATE-/{f=1} f;/-BEGIN CERTIFICATE-/{exit}' |
+		tac |
+		openssl x509 -noout -subject -issuer
+}
