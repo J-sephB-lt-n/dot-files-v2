4d40d0de51fa7fb61044220dc2925732b17c6fe8
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-01-06 12:41:35 +0200

    fix(.bash_aliases_v2.sh) Removed hard-coded domain from checkcert() function

--- .bash_aliases_v2.sh
+++ .bash_aliases_v2.sh
@@ -2,11 +2,11 @@
 alias poetry_run_pylint_recursive='poetry run pylint --rcfile .pylintrc --recursive=y .'
 
 checkcert() {
-	# e.g. checkcert www.github.com
+	# e.g. checkcert www.google.com
 	# this function from: https://stackoverflow.com/questions/21181231/server-certificate-verification-failed-cafile-etc-ssl-certs-ca-certificates-c/67698986#67698986
 	echo -n |
-		openssl s_client -showcerts -servername www.github.com \
-			-connect www.github.com:443 2>/dev/null |
+		openssl s_client -showcerts -servername $1 \
+			-connect $1:443 2>/dev/null |
 		tac |
 		awk '/-END CERTIFICATE-/{f=1} f;/-BEGIN CERTIFICATE-/{exit}' |
 		tac |
