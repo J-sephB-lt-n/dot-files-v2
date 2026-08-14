6742badae5b5ad81b9b80310ee4c85943cc5079b
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2026-08-14 14:23:20 +0200

    feat(command_line_tools/llmctx.sh) working scaffold

---
+++ command_line_tools/llmctx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# To make globally available:
+#   1. chmod +x llmctx.sh
+#   2. ln -s /home/josephbbolton/command_line_tools/llmctx.sh ~/.local/bin/llmctx
+#   3. now you can run `llmctx --help` from any folder
+
+echo 'working'
