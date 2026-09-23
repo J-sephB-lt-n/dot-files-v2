ed1b335b9e2fdc9efbcbd8621c670fbd0e253fa9
Joseph Bolton <joseph.jazz.bolton@gmail.com>
2026-09-23 14:25:17 +0200

    feat(command_line_tools/llmctx.sh) moved to generic /home/jbolton directory

--- command_line_tools/llmctx.sh
+++ command_line_tools/llmctx.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # To make globally available:
 #   1. chmod +x llmctx.sh
-#   2. ln -s /home/josephbbolton/command_line_tools/llmctx.sh ~/.local/bin/llmctx
+#   2. ln -s $HOME/command_line_tools/llmctx.sh ~/.local/bin/llmctx
 #   3. now you can run `llmctx --help` from any folder
 
 LLMCTX_VERSION='
